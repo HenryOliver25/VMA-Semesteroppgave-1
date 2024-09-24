27f703718912fac14b2092e8f977bcaea07c5522
HenryOliver25 <mrstone605.ho@gmail.com>
2024-09-24 12:30:07 +0200

    tester git se

--- semesteroppgave1.py
+++ semesteroppgave1.py
@@ -3,7 +3,10 @@ import pandas as pd
 import openpyxl
 from openpyxl import Workbook, load_workbook
 
-# testtest
+# testtes
+# prøver meg igjen
+
+print("jaja")
 
 excel_file = "Assignment_1_data.xlsx"
 dataSet = pd.read_excel(excel_file)
