ce5c7203f030c91a74d73cf716d40415294175ba
HenryOliver25 <mrstone605.ho@gmail.com>
2024-09-24 12:37:54 +0200

    instruksjoner bruk av git

--- semesteroppgave1.py
+++ semesteroppgave1.py
@@ -1,3 +1,16 @@
+# Semesteroppgave 1 i VMA
+# Bruker git (github.com) til fildeling
+
+# LAGRE ENDRINGRER TIL GITHUB: 
+# Åpne terminal i VSCode -> Git bash
+# $ git add . (legger til alle filer)
+# $ git commit -m "din besked/hva du har endret" (legger til beskjed)
+# $ git push -u origin main (Pusher endringer til github)
+
+# LAST NED ENDRINGER FRA GITHUB:
+# Åpne terminal i VSCode -> Git bash
+# $ git pull (henter evt. endringer)
+
 import numpy as np
 import pandas as pd
 import openpyxl
