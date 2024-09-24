3c540a6457e4d2093679501cdaf7334a920ce6e2
Kristoffer Gjuvsland <kristoffergjuvsland@dhcp-10-22-85-175.wlan.ntnu.no>
2024-09-24 17:11:31 +0200

    oppg. b, prøver litt frem med i vektoren

--- semesteroppgave1.py
+++ semesteroppgave1.py
@@ -23,18 +23,39 @@ dataSet = pd.read_excel("Assignment_1_data.xlsx")
 #   a) 
 #
 
-ex = pd.DataFrame([3, 4, 5, 7, 9])
+ex = pd.DataFrame([3, 4, 5, 7, 9]).mean()
 
+
+mean_daily_return_OSEBX=dataSet["OSEBX"].mean()
+mean_daily_return_EQUINOR=dataSet["EQUINOR"].mean()
 #print(dataSet["Date"] )
 # print(ex.mean())
-print("Mean dealy return OSEBX: ", dataSet["OSEBX"].mean())
-print("Mean dealy return EQUINOR: ", dataSet["EQUINOR"].mean())
+print("Mean dealy return OSEBX: ", mean_daily_return_OSEBX)
+print("Mean dealy return EQUINOR: ", mean_daily_return_EQUINOR)
+
+#mean_annual_return_OSEBX=(((1+mean_daily_return_OSEBX)*4044)/252)
+#print(mean_annual_return)
+
+
+
+
 
 
 # summarizing all daily returns into array of total annual return: annual return
 # when "some variable" equals new year, start new element in array which is summerized as the next 252 days
 # return 2001: 137 trading days
 # returns the mean annual return using .mean() functio
+
+
+
+#b
+#lager en i vektor som er N-dimensjonal og kun består av 1ere
+i=np.ones(len(dataSet.columns))
+print(i)
+
+
+
+
 
 
 
